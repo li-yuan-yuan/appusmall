1340cbbd31641ec23b31211704bf528e078317fe
liyuanyuan <2247722161@qq.com>
2020-08-06 20:12:04 +0800

    0806 第1次上传 完成目录创建

---
+++ src/components/Del.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Del() {
+    return (
+        <div>
+            确定删除
+        </div>
+    )
+}
---
+++ src/pages/Index/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div>
+            导航
+        </div>
+    )
+}
